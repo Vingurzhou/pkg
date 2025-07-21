daba74ea5b4b0b2b302986c6d64957718999d6ec
周文喆 <zwz0123460218@icloud.com>
2025-07-21 16:55:18 +0800

    chore: configure GORM logger to silent mode for cleaner output

--- db/db.go
+++ db/db.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 type GormConfig struct {
@@ -12,6 +13,7 @@ type GormConfig struct {
 func NewGormDB(dialector gorm.Dialector) *gorm.DB {
 	gormDb, err := gorm.Open(dialector, &gorm.Config{
 		TranslateError: true,
+		Logger:         logger.Default.LogMode(logger.Silent),
 	})
 	if err != nil {
 		fmt.Println(err)
